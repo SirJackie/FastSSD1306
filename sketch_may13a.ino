3093493681e71f20aee3d8a74c4991f1773f08ce
lyj00912 <3122742760@qq.com>
2021-05-14 20:39:48 +0800

    created multi-commands and multi-data sending functions

--- sketch_may13a.ino
+++ sketch_may13a.ino
@@ -7,42 +7,63 @@ void WriteCommand(char command){
   Wire.endTransmission();
 }
 
-void WriteData(char data){
+void WriteDatum(char data){
   Wire.beginTransmission(0x3C);
   Wire.write(0x40);
   Wire.write(data);
   Wire.endTransmission();
 }
 
+void StartWirtingMultiCommands(){
+  Wire.beginTransmission(0x3C);
+  Wire.write(0x80);
+}
+
+void StartWirtingMultiData(){
+  Wire.beginTransmission(0x3C);
+  Wire.write(0xC0);
+}
+
+void WriteMultiCommands(char command){
+  Wire.write(command);
+}
+
+void EndWritingMultiThings(){
+  Wire.endTransmission();
+}
+
 void OledInit(){
-  WriteCommand(0xAE);//--turn off oled panel
-  WriteCommand(0x00);//---set low column address
-  WriteCommand(0x10);//---set high column address
-  WriteCommand(0x40);//--set start line address  Set Mapping RAM Display Start Line (0x00~0x3F)
-  WriteCommand(0x81);//--set contrast control register
-  WriteCommand(0xCF); // Set SEG Output Current Brightness
-  WriteCommand(0xA1);//--Set SEG/Column Mapping     0xa0左右反置 0xa1正常
-  WriteCommand(0xC8);//Set COM/Row Scan Direction   0xc0上下反置 0xc8正常
-  WriteCommand(0xA6);//--set normal display
-  WriteCommand(0xA8);//--set multiplex ratio(1 to 64)
-  WriteCommand(0x3f);//--1/64 duty
-  WriteCommand(0xD3);//-set display offset        Shift Mapping RAM Counter (0x00~0x3F)
-  WriteCommand(0x00);//-not offset
-  WriteCommand(0xd5);//--set display clock divide ratio/oscillator frequency
-  WriteCommand(0x80);//--set divide ratio, Set Clock as 100 Frames/Sec
-  WriteCommand(0xD9);//--set pre-charge period
-  WriteCommand(0xF1);//Set Pre-Charge as 15 Clocks & Discharge as 1 Clock
-  WriteCommand(0xDA);//--set com pins hardware configuration
-  WriteCommand(0x12);
-  WriteCommand(0xDB);//--set vcomh
-  WriteCommand(0x40);//Set VCOM Deselect Level
-  WriteCommand(0x20);//-Set Page Addressing Mode (0x00/0x01/0x02)
-  WriteCommand(0x02);//
-  WriteCommand(0x8D);//--set Charge Pump enable/disable
-  WriteCommand(0x14);//--set(0x10) disable
-  WriteCommand(0xA4);
-  WriteCommand(0xA6);
-  WriteCommand(0xAF);
+
+  StartWirtingMultiCommands();
+  WriteMultiCommands(0xAE);//--turn off oled panel
+  WriteMultiCommands(0x00);//---set low column address
+  WriteMultiCommands(0x10);//---set high column address
+  WriteMultiCommands(0x40);//--set start line address  Set Mapping RAM Display Start Line (0x00~0x3F)
+  WriteMultiCommands(0x81);//--set contrast control register
+  WriteMultiCommands(0xCF); // Set SEG Output Current Brightness
+  WriteMultiCommands(0xA1);//--Set SEG/Column Mapping     0xa0左右反置 0xa1正常
+  WriteMultiCommands(0xC8);//Set COM/Row Scan Direction   0xc0上下反置 0xc8正常
+  WriteMultiCommands(0xA6);//--set normal display
+  WriteMultiCommands(0xA8);//--set multiplex ratio(1 to 64)
+  WriteMultiCommands(0x3f);//--1/64 duty
+  WriteMultiCommands(0xD3);//-set display offset        Shift Mapping RAM Counter (0x00~0x3F)
+  WriteMultiCommands(0x00);//-not offset
+  WriteMultiCommands(0xd5);//--set display clock divide ratio/oscillator frequency
+  WriteMultiCommands(0x80);//--set divide ratio, Set Clock as 100 Frames/Sec
+  WriteMultiCommands(0xD9);//--set pre-charge period
+  WriteMultiCommands(0xF1);//Set Pre-Charge as 15 Clocks & Discharge as 1 Clock
+  WriteMultiCommands(0xDA);//--set com pins hardware configuration
+  WriteMultiCommands(0x12);
+  WriteMultiCommands(0xDB);//--set vcomh
+  WriteMultiCommands(0x40);//Set VCOM Deselect Level
+  WriteMultiCommands(0x20);//-Set Page Addressing Mode (0x00/0x01/0x02)
+  WriteMultiCommands(0x02);//
+  WriteMultiCommands(0x8D);//--set Charge Pump enable/disable
+  WriteMultiCommands(0x14);//--set(0x10) disable
+  WriteMultiCommands(0xA4);
+  WriteMultiCommands(0xA6);
+  WriteMultiCommands(0xAF);
+  EndWritingMultiThings();
 }
 
 unsigned char FrameBuffer[8][128] = {0};
@@ -56,7 +77,7 @@ void RefreshBuffer(){
     WriteCommand(0x00);      //设置显示位置—列低地址
     WriteCommand(0x10);      //设置显示位置—列高地址
     for (n = 0; n < 128; n++){
-      WriteData(FrameBuffer[i][n]);
+      WriteDatum(FrameBuffer[i][n]);
     }
   }
 }
